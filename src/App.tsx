ff98f62f208264d2c2423920c7916086cf156e4f
DePacos <depacos@yandex.ru>
2024-03-28 23:16:05 +0300

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Todolist} from "./components/Todolist";
-
-const style = {
-    'display': 'flex',
-    'gap': '20px'
-}
-
-export type PropsTask = {
-    id: number,
-    title: string,
-    isDone: boolean,
-}
+import {data} from "./redux/data";
 
 export const App = () => {
     return (
-        <div className="App" style={style}>
-            <Todolist title="What to learn" propsTask={tasks1}/>
-            <Todolist data="03/05/2005" title="Songs" propsTask={tasks2}/>
+        <div className="App">
+            <Todolist title="What to learn" propsTask={data.tasks1}/>
+            <Todolist title="Songs" propsTask={data.tasks2} data="03/05/2005"/>
             <Todolist title="Books" propsTask={[]}/>
         </div>
     );
 }
-
-const tasks1: PropsTask[] = [
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
-]
-
-const tasks2 = [
-    { id: 1, title: 'Hello world', isDone: true },
-    { id: 2, title: 'I am Happy', isDone: false },
-    { id: 3, title: 'Yo', isDone: false },
-]
