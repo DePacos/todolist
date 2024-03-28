70449d01cd44004978d3c6955a5648ab3b56fa0c
DePacos <depacos@yandex.ru>
2024-03-28 23:19:58 +0300

    adding todolist functionality

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,41 +1,82 @@
 import React from 'react';
-
-import {PropsTask} from "../App";
 import {Button} from "./Button";
+import {S} from "./Todolist_Styled";
+
+export type TaskProps = {
+    id: number,
+    title: string,
+    isDone: boolean,
+}
 
 type TodolistProps = {
     title: string,
-    propsTask: PropsTask[],
+    propsTask: Array<TaskProps>,
     data?: string,
 }
 
-export const Todolist = (props: TodolistProps) => {
+type Filter = 'all' | 'active' | 'completed'
+
+export const Todolist = ({title, propsTask, data}: TodolistProps) => {
+    const [inputValue, setInputValue] = React.useState('')
+    const [tasks, setTasks] = React.useState<Array<TaskProps>>(propsTask)
+    const [tasksFilter, setTasksFilter] = React.useState<Filter>('all')
+
+    const checkInputValue = (e:any) => {
+            setInputValue(e.target.value)
+    }
+
+    const addTask = () => {
+    let newTasks = [...tasks]
+        newTasks.push({id: tasks.length + 1, title: inputValue, isDone: false})
+        setTasks(newTasks)
+    }
+
+    const removeTask = (id:number) => {
+        const changeTasks = tasks.filter((item) => item.id !== id)
+        setTasks(changeTasks)
+    }
+
+    const visibleTasks = () =>{
+        if(tasksFilter === 'active'){
+            return tasks.filter(item => item.isDone === false)
+        }
+        if(tasksFilter === 'completed'){
+            return tasks.filter(item => item.isDone === true)
+        }
+        return tasks
+    }
+
     return (
-        <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            { (props.propsTask.length !== 0 ?
-                    props.propsTask.map((item) => {
-                        return (
-                            <ul>
+        <S.TodolistWrap>
+            <h3>{title}</h3>
+            <S.InputWrap>
+                <input  onChange={e => checkInputValue(e)} value={inputValue}/>
+                <Button onClick={addTask}>+</Button>
+            </S.InputWrap>
+            <ul>
+                {(visibleTasks().length !== 0 ?
+                        visibleTasks().map(item => {
+                            return (
                                 <li key={item.id}>
-                                    <input type="checkbox" checked={item.isDone}/> <span>{item.title}</span>
+                                    <input
+                                        type="checkbox"
+                                        checked={item.isDone}
+                                        />
+                                    <span>{item.title}</span>
+                                    <Button onClick={() => removeTask(item.id)}>X</Button>
                                 </li>
-                            </ul>
-                        )
-                    })
-                : <span>No Tasks</span>
-            )
-            }
-            {props.data}
-            <div>
-                <Button>All</Button>
-                <Button>Active</Button>
-                <Button>Completed</Button>
-            </div>
-        </div>
+                            )
+                        })
+                        : <span>No Tasks</span>
+                )
+                }
+            </ul>
+            {(data) ? <div>{data}</div> : null}
+            <S.ButtonWrap>
+                <Button onClick={() => setTasksFilter('all')}>All</Button>
+                <Button onClick={() => setTasksFilter('active')}>Active</Button>
+                <Button onClick={() => setTasksFilter('completed')}>Completed</Button>
+            </S.ButtonWrap>
+        </S.TodolistWrap>
     );
 }
