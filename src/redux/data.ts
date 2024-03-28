8bf6eccd21dc7084f42ef87136a0c681ad116d54
DePacos <depacos@yandex.ru>
2024-03-28 23:17:10 +0300

    data file

---
+++ src/redux/data.ts
@@ -0,0 +1,13 @@
+export const data = {
+    tasks1: [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+    ],
+    tasks2: [
+        { id: 1, title: 'Hello world', isDone: true },
+        { id: 2, title: 'I am Happy', isDone: false },
+        { id: 3, title: 'Yo', isDone: false },
+    ]
+}
