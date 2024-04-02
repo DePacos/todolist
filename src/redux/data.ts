e60dc817daac861294ee6cfc0b981087091447b6
DePacos <depacos@yandex.ru>
2024-04-02 19:00:33 +0300

    changing data array

--- src/redux/data.ts
+++ src/redux/data.ts
@@ -1,13 +1,31 @@
-export const data = {
-    tasks1: [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-        {id: 4, title: 'Redux', isDone: false},
-    ],
-    tasks2: [
-        { id: 1, title: 'Hello world', isDone: true },
-        { id: 2, title: 'I am Happy', isDone: false },
-        { id: 3, title: 'Yo', isDone: false },
-    ]
-}
+export const data = [
+    {   id: 1,
+        title: 'What to learn',
+        date: '',
+        tasks: [
+            {id: 1, title: 'HTML&CSS', isDone: true},
+            {id: 2, title: 'JS', isDone: true},
+            {id: 3, title: 'React', isDone: false},
+            {id: 4, title: 'Redux', isDone: false},
+        ],
+        tasksFilter: 'all'
+    },
+    {
+        id: 2,
+        title: 'Phrase',
+        date: '01/10/2020',
+        tasks: [
+            {id: 1, title: 'Hello world', isDone: true},
+            {id: 2, title: 'I am Happy', isDone: false},
+            {id: 3, title: 'Yo', isDone: false},
+        ],
+        tasksFilter: 'all'
+    },
+    {
+        id: 3,
+        title: 'Books',
+        date: '',
+        tasks: [],
+        tasksFilter: 'all',
+    },
+]
