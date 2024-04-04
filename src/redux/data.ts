0a7719bf1b7c0bfc77729b63306336e2a4bd8ea6
DePacos <depacos@yandex.ru>
2024-04-04 23:50:49 +0300

    added generator id

--- src/redux/data.ts
+++ src/redux/data.ts
@@ -1,12 +1,14 @@
+import {v1} from "uuid";
+
 export const data = [
     {   id: 1,
         title: 'What to learn',
         date: '',
         tasks: [
-            {id: 1, title: 'HTML&CSS', isDone: true},
-            {id: 2, title: 'JS', isDone: true},
-            {id: 3, title: 'React', isDone: false},
-            {id: 4, title: 'Redux', isDone: false},
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'React', isDone: false},
+            {id: v1(), title: 'Redux', isDone: false},
         ],
         tasksFilter: 'all'
     },
@@ -15,9 +17,9 @@ export const data = [
         title: 'Phrase',
         date: '01/10/2020',
         tasks: [
-            {id: 1, title: 'Hello world', isDone: true},
-            {id: 2, title: 'I am Happy', isDone: false},
-            {id: 3, title: 'Yo', isDone: false},
+            {id: v1(), title: 'Hello world', isDone: true},
+            {id: v1(), title: 'I am Happy', isDone: false},
+            {id: v1(), title: 'Yo', isDone: false},
         ],
         tasksFilter: 'all'
     },
