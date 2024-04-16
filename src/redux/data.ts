bcb54ec6324eab3cfb65eeed404a2bfb1c954876
DePacos <depacos@yandex.ru>
2024-04-16 22:54:03 +0300

    added new data

--- src/redux/data.ts
+++ src/redux/data.ts
@@ -1,7 +1,8 @@
 import {v1} from "uuid";
 
-export const data = [
-    {   id: 1,
+export const
+    data = [
+    {   id: v1(),
         title: 'What to learn',
         date: '',
         tasks: [
@@ -13,7 +14,7 @@ export const data = [
         tasksFilter: 'all'
     },
     {
-        id: 2,
+        id: v1(),
         title: 'Phrase',
         date: '01/10/2020',
         tasks: [
@@ -24,10 +25,39 @@ export const data = [
         tasksFilter: 'all'
     },
     {
-        id: 3,
+        id: v1(),
         title: 'Books',
         date: '',
         tasks: [],
         tasksFilter: 'all',
     },
 ]
+
+
+let idTodo1 = v1()
+let idTodo2 = v1()
+let idTodo3 = v1()
+
+export const todoDataLists = [
+        { id: idTodo1, title: 'What to learn', date: '', tasksFilter: 'all'},
+        { id: idTodo2, title: 'Phrase', date: '01/10/2020', tasksFilter: 'all'},
+        { id: idTodo3, title: 'Books', date: '', tasksFilter: 'all'},
+    ]
+
+
+export const tasksDataLists = {
+    [idTodo1]:[
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+        {id: v1(), title: 'Redux', isDone: false},
+    ],
+    [idTodo2]:[
+        {id: v1(), title: 'Hello world', isDone: true},
+        {id: v1(), title: 'I am Happy', isDone: false},
+        {id: v1(), title: 'Yo', isDone: false},
+    ],
+    [idTodo3]:[]
+}
+
+
