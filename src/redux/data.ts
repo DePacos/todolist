a1542bccc317d4187a93c3251b33773e533789d9
DePacos <depacos@yandex.ru>
2024-05-23 22:34:04 +0300

    change task id for test

--- src/redux/data.ts
+++ src/redux/data.ts
@@ -38,6 +38,21 @@ let idTodo1 = v1()
 let idTodo2 = v1()
 let idTodo3 = v1()
 
+// for tests
+
+export let idTasks1 = {
+    id1: v1(),
+    id2: v1(),
+    id3: v1(),
+    id4: v1(),
+}
+
+export let idTasks2 = {
+    id1: v1(),
+    id2: v1(),
+    id3: v1(),
+}
+
 export const todoDataLists = [
         { id: idTodo1, title: 'What to learn', date: '', tasksFilter: 'all'},
         { id: idTodo2, title: 'Phrase', date: '01/10/2020', tasksFilter: 'all'},
@@ -47,15 +62,15 @@ export const todoDataLists = [
 
 export const tasksDataLists = {
     [idTodo1]:[
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'React', isDone: false},
-        {id: v1(), title: 'Redux', isDone: false},
+        {id: idTasks1.id1, title: 'HTML&CSS', isDone: true},
+        {id: idTasks1.id2, title: 'JS', isDone: true},
+        {id: idTasks1.id3, title: 'React', isDone: false},
+        {id: idTasks1.id4, title: 'Redux', isDone: false},
     ],
     [idTodo2]:[
-        {id: v1(), title: 'Hello world', isDone: true},
-        {id: v1(), title: 'I am Happy', isDone: false},
-        {id: v1(), title: 'Yo', isDone: false},
+        {id: idTasks2.id1, title: 'Hello world', isDone: true},
+        {id: idTasks2.id2, title: 'I am Happy', isDone: false},
+        {id: idTasks2.id3, title: 'Yo', isDone: false},
     ],
     [idTodo3]:[]
 }
