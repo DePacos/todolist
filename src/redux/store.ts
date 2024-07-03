89a460763514ada4130b7116be6a25ab27db82ef
DePacos <depacos@yandex.ru>
2024-07-03 23:19:48 +0300

    added type AppThunkDispatch, applyMiddleware, ThunkDispatch, thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,22 @@
-import {combineReducers, createStore} from "redux";
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore} from "redux";
 import {todolistReducer} from "../model/todolist-reducer";
 import {taskReducer} from "../model/task-reducer";
+import {thunk, ThunkDispatch} from "redux-thunk";
+import {useDispatch} from "react-redux";
 
+
+
+export type AppRootState = ReturnType<typeof rootReducer>
+export type AppThunkDispatch = ThunkDispatch<AppRootState, any, AnyAction>
 
 const rootReducer = combineReducers({
     todolists: todolistReducer,
     tasks: taskReducer,
 })
 
-export type AppRootState = ReturnType<typeof rootReducer>
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk) as any)
 
-export const store = createStore(rootReducer)
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 
 // @ts-ignore
 window.store = store
