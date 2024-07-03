61adef1cf2e3cfb53e65b99eef0cc0cbeede1e40
DePacos <depacos@yandex.ru>
2024-07-03 23:21:41 +0300

    added new task object

--- src/components/Task.stories.tsx
+++ src/components/Task.stories.tsx
@@ -15,7 +15,18 @@ const meta: Meta<typeof Task> = {
         changeTaskStatusHandler: action('Status changed inside Task'),
         changeTitleTaskHandler: action('Title changed inside Task'),
         removeTaskHandler: action('Remove Button clicked changed inside Task'),
-        task: {id: '123', title: 'JS', isDone: false},
+        task: {
+            id: 'id',
+            title: 'title',
+            description: 'desc',
+            todoListId: 'todoID',
+            order: -5,
+            status: 5,
+            priority: 7,
+            startDate: 'startDate',
+            deadline: 'deadline',
+            addedDate: 'addedDate'
+        }
     }
 };
 
@@ -27,6 +38,17 @@ export const TaskIsNotDoneStory: Story = {};
 export const TaskIsDoneStory: Story = {
 
     args: {
-        task: {id: '123', title: 'CSS', isDone: true},
+        task: {
+            id: 'id',
+            title: 'title',
+            description: 'desc',
+            todoListId: 'todoID',
+            order: -5,
+            status: 5,
+            priority: 7,
+            startDate: 'startDate',
+            deadline: 'deadline',
+            addedDate: 'addedDate'
+        }
     },
 };
