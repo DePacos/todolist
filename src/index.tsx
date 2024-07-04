077cd1a4ecb8c595f4e1b759bd5bfc6d5a186224
DePacos <depacos@yandex.ru>
2024-07-04 23:08:50 +0300

    change imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/assets/fonts/fonts.css'
-import {App} from './App';
+import {App} from './app/App';
 import {Provider} from "react-redux";
-import {store} from "./redux/store";
+import {store} from "./store/store";
 
 
 
