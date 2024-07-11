f3b7aa952ccf0c6be5396c91821d66afea3e65b0
DePacos <depacos@yandex.ru>
2024-07-11 22:46:00 +0300

    added RouterProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/assets/fonts/fonts.css'
-import {App} from './app/App';
 import {Provider} from "react-redux";
 import {store} from "./store/store";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routers/routers";
 
 
 
@@ -14,7 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App/>
+            <RouterProvider router={router}/>
         </Provider>
     </React.StrictMode>
 ); 
