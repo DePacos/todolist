1260b16b4f49268533827e1bdb152fb4e798e989
DePacos <depacos@yandex.ru>
2024-07-15 23:12:26 +0300

    refactor

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -3,17 +3,12 @@ import {ResponseType} from "../api/todolistsAPI"
 import {Dispatch} from "redux";
 
 
-
-export const handleServerAppError = <D> (data: ResponseType<D>, dispatch: Dispatch<SetAppErrorType | SetAppStatusType>) => {
-    if (data.messages.length) {
-        dispatch(setAppErrorAC(data.messages[0]))
-    } else {
-        dispatch(setAppErrorAC('Some error occurred'))
-    }
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorType | SetAppStatusType>) => {
+    dispatch(setAppErrorAC(data.messages.length ? data.messages[0] : 'Some error occurred'))
     dispatch(setAppStatusAC('failed'))
 }
 
-export const handleServerNetworkError = (error: {message: string}, dispatch: Dispatch<SetAppErrorType | SetAppStatusType>) => {
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorType | SetAppStatusType>) => {
     dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
     dispatch(setAppStatusAC('failed'))
 }
