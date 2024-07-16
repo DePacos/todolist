cbe70e2c6b9b5f4f0fcfbcc265e27b853faad938
DePacos <depacos@yandex.ru>
2024-07-16 23:15:04 +0300

    adding checking isInitialized, routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Header} from "../components/Header";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import {TodoListsList} from "../features/Todolists/TodoListsList";
+import {Outlet} from "react-router-dom";
+import {AppRootState, useAppDispatch} from "../store/store";
+import {meTC} from "../store/reducers/auth-reducer";
+import {CircularProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import Box from "@mui/material/Box";
+import {SM} from "../styles/material-styles";
 
 
 export const App = () => {
+    const isInitialized = useSelector<AppRootState, boolean>(state => state.app.isInitialized)
     const [themeMode, setThemeMode] = React.useState<ThemeMode>('light')
     const changeModeHandler = React.useCallback(() => {
         setThemeMode(themeMode === 'light' ? 'dark' : 'light')
     }, [themeMode])
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(meTC())
+    }, [])
 
     const theme = React.useMemo(() => {
         return createTheme({
@@ -19,16 +32,27 @@ export const App = () => {
                 primary: {
                     main: '#087EA4',
                 },
+                secondary: {
+                    main: '#26843f',
+                },
             },
         })
     }, [themeMode])
+
+    if (!isInitialized) {
+        return (
+            <Box sx={SM.wrapCircularProgress}>
+                <CircularProgress />
+            </Box>
+        )
+    }
 
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
             <div className="App">
                 <Header theme={theme} changeModeHandler={changeModeHandler}/>
-                <TodoListsList/>
+                <Outlet/>
             </div>
         </ThemeProvider>
     );
