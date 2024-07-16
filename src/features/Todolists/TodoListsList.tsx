43acd7953840e4349fea4fe44ec97f67d11c40e8
DePacos <depacos@yandex.ru>
2024-07-16 22:57:43 +0300

    added checking isLogged

--- src/features/Todolists/TodoListsList.tsx
+++ src/features/Todolists/TodoListsList.tsx
@@ -1,28 +1,35 @@
 import {useSelector} from "react-redux";
 import {AppRootState, useAppDispatch} from "../../store/store";
-import {createTodolistTC, TodoListsDomainType, todoListTC} from "../../store/reducers/todolist-reducer";
+import {createTodolistTC, TodoListsDomainType, getTodoListTC} from "../../store/reducers/todolist-reducer";
 import {Todolist} from "./Todolist";
 import React, {useEffect} from "react";
 import {AddItemForm} from "../../components/AddItemForm/AddItemForm";
 import {SM} from "../../styles/material-styles";
 import Box from "@mui/material/Box";
+import {Navigate} from "react-router-dom";
+
 
 export const TodoListsList = React.memo(() => {
     const todoLists = useSelector<AppRootState, TodoListsDomainType[]>(state => state.todolists)
+    const isLoggedIn = useSelector<AppRootState, boolean>(state => state.auth.isLoggedIn)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
-        dispatch(todoListTC())
+        if (!isLoggedIn) return
+        dispatch(getTodoListTC())
+
     }, [])
 
     const createTodolist = React.useCallback((titleTodolist: string) => {
         dispatch(createTodolistTC(titleTodolist))
     }, [])
 
+    if (!isLoggedIn) return <Navigate to={'/login'} />
+
     return (
         <>
             <Box sx={SM.wrapAddTodo}>
-                <AddItemForm addItem={createTodolist}/>
+                <AddItemForm addItem={createTodolist} inputLabel='Todolist name'/>
             </Box>
             <Box sx={SM.wrapTodoLists}>
                 {
@@ -35,6 +42,7 @@ export const TodoListsList = React.memo(() => {
                                     date={todo.addedDate}
                                     order={todo.order}
                                     tasksFilter={todo.tasksFilter}
+                                    entityStatus={todo.entityStatus}
                                 />
                             )
                         }
