142c6bfff3a5741f6b2115c8cacfeb06c6c0f088
DeBook <depacos1@gmail.com>
2024-07-31 22:04:16 +0300

    replacing app component with todo component

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -1,29 +1,23 @@
-import type {Meta, StoryObj}   from '@storybook/react';
-import {App} from "./App";
-import { ReduxStoreProviderDecorator } from '../stories/decorators/ReduxStoreProviderDecorator';
+import type { Meta, StoryObj } from "@storybook/react"
+import React from "react"
+import { App } from "./App"
+import { ReduxStoreProviderDecorator } from "../stories/decorators/ReduxStoreProviderDecorator"
+import { TodoListsList } from "../features/Todolists/TodoListsList"
 
-// More on how to set up stories at:
-// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof TodoListsList> = {
+  title: "TODOLIST/App",
+  component: TodoListsList,
+  tags: ["autodocs"],
+  decorators: [ReduxStoreProviderDecorator],
+}
 
-const meta: Meta<  typeof App> = {
-    title:   'TODOLISTS/App',
-    component:   App,
+export default meta
+type Story = StoryObj<typeof TodoListsList>
 
-
-// This component will have an automatically generated Autodocs entry:
-// https://storybook.js.org/docs/react/writing-docs/autodocs
-
-    tags: [  'autodocs'],
-
-// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-
-    decorators: [ReduxStoreProviderDecorator]
-};
-
-export default meta;
-type Story = StoryObj<  typeof App>;
-
-// More on component templates:
-// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-
-export const AppWithReduxStory: Story = {}
+export const AppBaseExample = () => {
+  return(
+    <>
+      <TodoListsList/>
+    </>
+  )
+}
