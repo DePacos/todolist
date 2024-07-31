7bbb086d7b633d19341f161f0ff73e3a9fcee21e
DeBook <depacos1@gmail.com>
2024-07-31 22:06:15 +0300

    added comments for property story

--- src/components/AddItemForm/AddItemForm.stories.tsx
+++ src/components/AddItemForm/AddItemForm.stories.tsx
@@ -1,40 +1,40 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import {AddItemForm} from "./AddItemForm";
-import {action} from '@storybook/addon-actions'
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+import type { Meta, StoryObj } from "@storybook/react"
+import { AddItemForm } from "./AddItemForm"
+import { action } from "@storybook/addon-actions"
 
 const meta = {
-    title: 'TODOLISTS/AddItemForm',
-    component: AddItemForm,
-    parameters: {
-        layout: 'centered',
-        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+  title: "TODOLIST/AddItemForm",
+  component: AddItemForm,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    addItem: {
+      description: "Button clicked inside form",
+      action: "clicked",
     },
-
-    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-
-    tags: ['autodocs'],
-
-    // More on argTypes: https://storybook.js.org/docs/api/argtypes
-
-    argTypes: {
-        addItem: {
-            description: 'Button clicked inside form',
-            action: 'clicked'
-        }
+    inputLabel: {
+      description: "Change input label",
     },
-
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+    disable: {
+      description: "On/Off disabled",
+    },
+  },
 }
 
-export default meta;
-type Story = StoryObj<typeof AddItemForm>;
-
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-
 export const AddItemFormStory: Story = {
-    args: {
-        addItem: action('Button clicked inside form')
-    },
-};
+  args: {
+    addItem: action("Button clicked inside form"),
+  },
+}
+
+export const AddItemFormDisableStory: Story = {
+  args: {
+    addItem: action("Button clicked inside form"),
+    disable: true,
+  },
+}
+
+type Story = StoryObj<typeof AddItemForm>
+export default meta
--- src/components/EditableSpan/EditableSpan.stories.tsx
+++ src/components/EditableSpan/EditableSpan.stories.tsx
@@ -1,32 +1,33 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import {action} from '@storybook/addon-actions'
-import { EditableSpan } from './EditableSpan';
-
-
+import type { Meta, StoryObj } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import { EditableSpan } from "./EditableSpan"
 
 const meta: Meta<typeof EditableSpan> = {
-    title: 'TODOLISTS/EditableSpan',
-    component: EditableSpan,
-    parameters: {
-        layout: 'centered',
+  title: "TODOLIST/EditableSpan",
+  component: EditableSpan,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    title: {
+      description: "Start value empty. Add value push button set string.",
     },
-    tags: ['autodocs'],
-    argTypes: {
-        title: {
-            description: 'Start value empty. Add value push button set string.'
-        },
-        onChange: {
-            description: 'Value EditableSpan changed'
-        }
-    }
-};
+    onChange: {
+      description: "Value EditableSpan changed",
+    },
+    disable: {
+      description: "On/Off disabled",
+    },
+  },
+}
 
-export default meta;
-type Story = StoryObj<typeof EditableSpan>;
+export default meta
+type Story = StoryObj<typeof EditableSpan>
 
 export const EditableSpanStory: Story = {
-    args: {
-        onChange: action('Value EditableSpan changed'),
-        title: 'test title'
-    }
-};
+  args: {
+    onChange: action("Value EditableSpan changed"),
+    title: "test title",
+  },
+}
