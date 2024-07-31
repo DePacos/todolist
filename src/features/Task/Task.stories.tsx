14e3fd377ef04bb09fa3f2e9e0cbad8afe129fdb
DeBook <depacos1@gmail.com>
2024-07-31 22:08:30 +0300

    added decorators and TaskIsNotDoneStory

--- src/features/Task/Task.stories.tsx
+++ src/features/Task/Task.stories.tsx
@@ -1,49 +1,64 @@
-import type {Meta, StoryObj} from '@storybook/react';
-import {Task} from './Task';
-
+import type { Meta, StoryObj } from "@storybook/react"
+import { Task } from "./Task"
+import { ReduxStoreProviderDecorator } from "../../stories/decorators/ReduxStoreProviderDecorator"
 
 const meta: Meta<typeof Task> = {
-    title: 'TODOLISTS/Task',
-    component: Task,
-    parameters: {
-        layout: 'centered',
+  title: "TODOLIST/Task",
+  component: Task,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [ReduxStoreProviderDecorator],
+  args: {
+    task: {
+     id: "id",
+      title: "task title",
+      description: "desc",
+      todoListId: "todoID",
+      order: -5,
+      status: 5,
+      priority: 7,
+      startDate: "startDate",
+      deadline: "deadline",
+      addedDate: "addedDate",
     },
-    tags: ['autodocs'],
-    args: {
-        task: {
-            id: 'id',
-            title: 'title',
-            description: 'desc',
-            todoListId: 'todoID',
-            order: -5,
-            status: 5,
-            priority: 7,
-            startDate: 'startDate',
-            deadline: 'deadline',
-            addedDate: 'addedDate'
-        }
-    }
-};
+  },
+}
 
-export default meta;
-type Story = StoryObj<typeof Task>;
+export default meta
+type Story = StoryObj<typeof Task>
 
-export const TaskIsNotDoneStory: Story = {};
+export const TaskIsNotDoneStory: Story = {
+  args: {
+    task: {
+      id: "id",
+      title: "no check",
+      description: "desc",
+      todoListId: "todoID",
+      order: -5,
+      status: 0,
+      priority: 7,
+      startDate: "startDate",
+      deadline: "deadline",
+      addedDate: "addedDate",
+    },
+  },
+}
 
 export const TaskIsDoneStory: Story = {
-
-    args: {
-        task: {
-            id: 'id',
-            title: 'title',
-            description: 'desc',
-            todoListId: 'todoID',
-            order: -5,
-            status: 5,
-            priority: 7,
-            startDate: 'startDate',
-            deadline: 'deadline',
-            addedDate: 'addedDate'
-        }
+  args: {
+    task: {
+      id: "id",
+      title: "check",
+      description: "desc",
+      todoListId: "todoID",
+      order: -5,
+      status: 1,
+      priority: 7,
+      startDate: "startDate",
+      deadline: "deadline",
+      addedDate: "addedDate",
     },
-};
+  },
+}
