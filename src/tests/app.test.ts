884c04447b76b7d3bb0553c8b2790b8e73368ba0
DeBook <depacos1@gmail.com>
2024-08-01 23:25:09 +0300

    refactor

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,24 +1,27 @@
-import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from "../store/reducers/app-reducers";
+import { appActions, appReducer, RequestStatusType } from "../app/appSlice"
 
-
-let initialState: InitialStateType
+let initialState: {
+  isInitialized: boolean
+  status: RequestStatusType
+  error: null | string
+}
 
 beforeEach(() => {
-    initialState = {
-        isInitialized: false,
-        status: 'idle',
-        error: null
-    }
+  initialState = {
+    isInitialized: false,
+    status: "idle",
+    error: null,
+  }
 })
 
-test('correct status should be set', () => {
-    const endState = appReducer(initialState, setAppStatusAC('failed'))
+test("correct status should be set", () => {
+  const endState = appReducer(initialState, appActions.setStatus({ status: "failed" }))
 
-    expect(endState.status).toBe('failed')
+  expect(endState.status).toBe("failed")
 })
 
-test('correct error message should be set', () => {
-    const endState = appReducer(initialState, setAppErrorAC('error'))
+test("correct error message should be set", () => {
+  const endState = appReducer(initialState, appActions.setError({ error: "error" }))
 
-    expect(endState.error).toBe('error')
+  expect(endState.error).toBe("error")
 })
