d24ef289eca49e57a0e2c6137e283d70cb8227ac
DeBook <depacos1@gmail.com>
2024-08-11 22:05:45 +0300

    added appMatchers, remove reducer serStatus and added error handling

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, isFulfilled, isPending, isRejected, PayloadAction } from "@reduxjs/toolkit"
+import { RejectActionError } from "common/types"
+import axios from "axios"
 
 export const sliceApp = createSlice({
   name: "app",
@@ -8,9 +10,6 @@ export const sliceApp = createSlice({
     error: null as null | string,
   },
   reducers: {
-    setStatus(state, action: PayloadAction<{ status: RequestStatusType }>) {
-      state.status = action.payload.status
-    },
     setError(state, action: PayloadAction<{ error: null | string }>) {
       state.error = action.payload.error
     },
@@ -18,10 +17,58 @@ export const sliceApp = createSlice({
       state.isInitialized = action.payload.isInitialized
     },
   },
+  extraReducers: builder => {
+    builder
+      .addMatcher(isPending, (state) => {
+          state.status = "loading"
+        }
+      )
+      .addMatcher(isFulfilled, (state) => {
+          state.status = "succeeded"
+        }
+      )
+      .addMatcher(isRejected, (state, action: any) => {
+          state.status = "failed"
+        }
+      )
+      .addMatcher(
+        (action): action is PayloadAction<RejectActionError> => {
+          return isRejected(action) && action.payload
+        },
+        (state, action: PayloadAction<RejectActionError>) => {
+          const defaultMessage = "Some error occurred"
+
+          switch (action.payload.type) {
+            case "appError": {
+              const error = action.payload.error
+              state.error = error.messages.length ? error.messages[0] : defaultMessage
+              break
+            }
+
+            case "catchError": {
+              const error = action.payload.error
+              if (axios.isAxiosError(error)) {
+                state.error = error.response?.data?.message || error?.message || defaultMessage
+              } else if (error instanceof Error) {
+                state.error = `Native error: ${error.message}`
+              } else {
+                state.error = JSON.stringify(error)
+              }
+              break
+            }
+
+            default:
+              state.error = defaultMessage
+          }
+        },
+      )
+      .addDefaultCase((state, action) => {
+        console.log(action)
+      })
+  }
 })
 
-export const appReducer = sliceApp.reducer
+export const appSlice = sliceApp.reducer
 export const appActions = sliceApp.actions
 
-// types
 export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed"
