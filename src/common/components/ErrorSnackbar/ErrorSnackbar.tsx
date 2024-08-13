e738e4701941aecca089c8eb55f678c1e6d6fe07
DeBook <depacos1@gmail.com>
2024-08-13 22:40:32 +0300

    moved from common components ErrorSnackbar

---
+++ src/common/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Snackbar from "@mui/material/Snackbar"
+import MuiAlert, { AlertProps } from "@mui/material/Alert"
+import { useDispatch, useSelector } from "react-redux"
+import { sliceApp } from "app/appSlice"
+import { selectAppError } from "app/appSelectors"
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+})
+
+export const ErrorSnackbar = () => {
+  const error = useSelector(selectAppError)
+  const dispatch = useDispatch()
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === "clickaway") {
+      return
+    }
+    dispatch(sliceApp.actions.setError({ error: null }))
+  }
+  return (
+    <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={"error"} sx={{ width: "100%" }}>
+        {error}
+      </Alert>
+    </Snackbar>
+  )
+}
