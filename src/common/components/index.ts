9c7440acee906380c730e4aecaa94acc9e876d13
DeBook <depacos1@gmail.com>
2024-08-14 23:07:46 +0300

    create index for components

---
+++ src/common/components/index.ts
@@ -0,0 +1,6 @@
+export { AddItemForm } from "common/components/AddItemForm/AddItemForm"
+export { EditableSpan } from "common/components/EditableSpan/EditableSpan"
+export { ErrorSnackbar } from "common/components/ErrorSnackbar/ErrorSnackbar"
+export {Header} from "common/components/Header/Header"
+export {BasicButton} from 'common/components/Buttons/BasicButton'
+export {MenuButton} from 'common/components/Buttons/MenuButton'
