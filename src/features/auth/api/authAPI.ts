4512f7c4249a41fe61f92515beae1775d5b67e65
DeBook <depacos1@gmail.com>
2024-08-15 23:20:30 +0300

    created authAPI

---
+++ src/features/auth/api/authAPI.ts
@@ -0,0 +1,23 @@
+import { BaseResponse } from "common/types"
+import { instance } from "common/instance"
+
+export const authAPI = {
+  login(data: LoginParams) {
+    return instance.post<BaseResponse<{ userId: number }>>(`/auth/login`, data)
+  },
+
+  logout() {
+    return instance.delete<BaseResponse<{ userId: number }>>(`/auth/login`)
+  },
+
+  me() {
+    return instance.get<BaseResponse<{ id: number, email: string, login: string}>>(`auth/me`)
+  },
+}
+
+export type LoginParams = {
+  email: string
+  password: string
+  rememberMe: boolean
+  captcha?: boolean
+}
