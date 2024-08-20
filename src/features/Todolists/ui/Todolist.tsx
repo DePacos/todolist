cff7a3ba59c5af0afd9abbcf7af7e0dd13c12c79
DeBook <depacos1@gmail.com>
2024-08-20 15:53:14 +0300

    delete console.log

--- src/features/Todolists/ui/Todolist.tsx
+++ src/features/Todolists/ui/Todolist.tsx
@@ -7,25 +7,22 @@ import { useAppDispatch } from "common/hooks"
 import { FilterTasksButtons } from "features/Todolists/ui/FilterTasksButtons"
 import { TodolistTitle } from "features/Todolists/ui/TodolistTitle"
 
-export const Todolist =
-  ({ todoId, title, date, tasksFilter, entityStatus }: Props) => {
-    const dispatch = useAppDispatch()
+export const Todolist = ({ todoId, title, date, tasksFilter, entityStatus }: Props) => {
+  const dispatch = useAppDispatch()
 
-    console.log('entityStatus', entityStatus)
+  useEffect(() => {
+    dispatch(fetchTasks(todoId))
+  }, [])
 
-    useEffect(() => {
-      dispatch(fetchTasks(todoId))
-    }, [])
-
-    return (
-      <Paper elevation={6} sx={SM.wrapTodoList}>
-       <TodolistTitle todoId={todoId} title={title} entityStatus={entityStatus}/>
-        <TasksList todoId={todoId} tasksFilter={tasksFilter} entityStatus={entityStatus} />
-        {date ? <div>{date}</div> : null}
-        <FilterTasksButtons todoId={todoId} tasksFilter={tasksFilter}/>
-      </Paper>
-    )
-  }
+  return (
+    <Paper elevation={6} sx={SM.wrapTodoList}>
+      <TodolistTitle todoId={todoId} title={title} entityStatus={entityStatus} />
+      <TasksList todoId={todoId} tasksFilter={tasksFilter} entityStatus={entityStatus} />
+      {date ? <div>{date}</div> : null}
+      <FilterTasksButtons todoId={todoId} tasksFilter={tasksFilter} />
+    </Paper>
+  )
+}
 
 type Props = {
   todoId: string
