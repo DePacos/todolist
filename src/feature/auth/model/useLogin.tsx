486cd56919e06987a713cf6facfc86097a5cca55
DePacos <depacos@yandex.ru>
2025-06-06 19:14:06 +0300

    refactor: login

--- src/feature/auth/model/useLogin.tsx
+++ src/feature/auth/model/useLogin.tsx
@@ -2,25 +2,21 @@ import { type SubmitHandler, useForm } from 'react-hook-form';
 
 import type { Login } from '@/types/types.ts';
 
+import { setToken } from '@/app/reducer/appSlice.ts';
 import { RESULTCODE } from '@/constants/resultCode.ts';
-import { useLoginMutation, useMeQuery } from '@/feature/auth/api/authApi.ts';
+import { useLoginMutation } from '@/feature/auth/api/authApi.ts';
+import { useAppDispatch } from '@/hooks/useAppDispatch.ts';
 
 export const useLogin = (handleOpenModal?: (isOpen: boolean) => void) => {
-  const { refetch } = useMeQuery();
+  const dispatch = useAppDispatch();
   const [login] = useLoginMutation();
-
-  const {
-    setError,
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<Login>();
+  const { control, setError, handleSubmit } = useForm<Login>();
 
   const onSubmit: SubmitHandler<Login> = async (data) => {
     const res = await login(data).unwrap();
     if (res.resultCode === RESULTCODE.success) {
       localStorage.setItem('access_token', res.data.token);
-      refetch();
+      dispatch(setToken(res.data.token));
       handleOpenModal?.(false);
     }
 
@@ -31,9 +27,8 @@ export const useLogin = (handleOpenModal?: (isOpen: boolean) => void) => {
   };
 
   return {
+    control,
     handleSubmit,
     onSubmit,
-    register,
-    errors,
   };
 };
