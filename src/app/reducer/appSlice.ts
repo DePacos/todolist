946ef578dcfebc1e8c80305211da14060e31efde
DePacos <depacos@yandex.ru>
2025-06-22 21:01:45 +0300

    feat(auth): handle auth token in slice

--- src/app/reducer/appSlice.ts
+++ src/app/reducer/appSlice.ts
@@ -3,9 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 export const appSlice = createSlice({
   name: 'app',
   initialState: {
-    theme: localStorage.getItem('theme') || ('light' as 'dark' | 'light'),
+    theme: localStorage.getItem('theme') || 'light',
     error: null as string | null,
     isAppLoading: false,
+    token: null as null | string,
   },
   reducers: {
     setTheme: (state, action) => {
@@ -17,13 +18,17 @@ export const appSlice = createSlice({
     setIsLoading: (state, action) => {
       state.isAppLoading = action.payload;
     },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
   },
   selectors: {
     selectTheme: (state) => state.theme,
     selectError: (state) => state.error,
     selectLoading: (state) => state.isAppLoading,
+    selectToken: (state) => state.token,
   },
 });
 
-export const { selectTheme, selectError, selectLoading } = appSlice.selectors;
-export const { setTheme, setError, setIsLoading } = appSlice.actions;
+export const { selectTheme, selectError, selectLoading, selectToken } = appSlice.selectors;
+export const { setTheme, setError, setIsLoading, setToken } = appSlice.actions;
