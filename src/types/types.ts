38d1f8a6784891f9dccb382db104804e72c1a754
DePacos <depacos@yandex.ru>
2025-10-14 18:06:04 +0300

    feat: types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,34 +1,63 @@
 import type { Control, FieldValues, Path } from 'react-hook-form';
 
-export type Response<Data> = {
-  resultCode: number;
-  messages: string[];
-  data: Data;
+import type { THEME_MODE, TASK_COLUMN, TASK_PRIORITY } from '@/constants';
+
+export type ThemeMode = (typeof THEME_MODE)[keyof typeof THEME_MODE];
+
+export type AppInitialState = {
+  theme: ThemeMode;
+  error: string | null;
+  isAppLoading: boolean;
 };
 
-export type Request = {
-  email: string;
-  password: string;
-  rememberMe?: boolean;
-};
+// shared
+export type Response<Data> = { resultCode: number; messages: string[]; data: Data };
+export type PutAfterItem = { putAfterItemId: string | null };
 
-export type ColumnType = {
-  id: string;
+// auth
+export type LoginResponse = { userId: number; token: string };
+export type LoginRequest = { email: string; password: string; rememberMe?: boolean };
+export type MeResponse = { userId: number; email: string };
+
+// boarder
+export type BoardId = { id: string };
+export type AddBoardRequest = { title: string };
+export type GetBoardsResponse = { addedDate: string; order: number } & BoardId & AddBoardRequest;
+export type UpdateBoardRequest = BoardId & AddBoardRequest;
+export type OrderBoardsRequest = PutAfterItem & BoardId;
+
+// task
+export type TaskId = { id: string };
+export type TaskItem = {
   title: string;
-  addedDate: string;
+  description: string;
+  completed: boolean;
+  status: number;
+  priority: TaskPriority;
+  startDate: string;
+  deadline: string;
   order: number;
 };
 
-export type Login = {
-  email: string;
-  password: string;
-  captcha?: string;
-  rememberMe?: boolean;
-};
+export type AddTaskRequest = Partial<TaskItem> & Pick<TaskItem, 'title'> & BoardId;
+export type UpdateTaskRequest = { taskId: string } & AddTaskRequest;
+export type OrderTaskRequest = { boardId: string; taskId: string } & PutAfterItem;
+export type DeleteTaskRequest = { boardId: string; taskId: string };
 
+export type TaskItemResponse = { todoListId: string; addedDate: string } & TaskItem & TaskId;
+export type TaskResponse = { items: TaskItemResponse[]; error: string; totalCount: number };
+
+// form
 export type FormField<T extends FieldValues> = {
-  rules?: Record<string, unknown>;
-  control: Control<T>;
   name: Path<T>;
+  control: Control<T>;
+  rules?: Record<string, unknown>;
   error?: string;
 };
+
+export type Option = { label: string; value: string };
+
+// constants
+export type TaskStatus = (typeof TASK_COLUMN)[keyof typeof TASK_COLUMN]['STATUS'];
+export type TaskColumnName = (typeof TASK_COLUMN)[keyof typeof TASK_COLUMN]['NAME'];
+export type TaskPriority = keyof typeof TASK_PRIORITY;
