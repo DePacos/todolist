c2938bbfa615b6e4962430152656aea0c88cf713
DePacos <depacos@yandex.ru>
2025-10-26 17:45:52 +0300

    fix: layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import 'react-loading-skeleton/dist/skeleton.css';
 
-import { DefaultLayout } from '@/app/layout';
 import { providers } from '@/providers/providers';
 import { ProviderComposer } from '@/providers/ProvidersComposer';
 
@@ -9,8 +8,4 @@ import '@/assets/fonts/fonts.css';
 import '@/styles/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <ProviderComposer providers={providers}>
-    <DefaultLayout />
-  </ProviderComposer>,
-);
+root.render(<ProviderComposer providers={providers} />);
