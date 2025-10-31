d14fe6ac85979efdb9abf48ea5f81aabb30203d3
depacos <depacos@yandex.ru>
2025-10-31 14:51:11 +0300

    fix: env name

--- src/app/api/todolistApi.ts
+++ src/app/api/todolistApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { ACCESS_TOKEN } from '@/constants';
 import { handleError } from '@/utils';
 
-const baseUrl = import.meta.env.VITE_BASE_API_URL || '';
+const baseUrl = import.meta.env.VITE_API_URL || '';
 const APIKEY = import.meta.env.VITE_API_KEY || '';
 
 export const todolistApi = createApi({
